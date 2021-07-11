3240bf013495e1dde8fbc31559c3fe8c2f0a0968
camizaato <hrkamizato@gmail.com>
2021-07-11 00:07:43 -0300

    first commit

---
+++ backend/src/test/java/xyz/hrkamiza/gsjulino/GsjulinoApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.hrkamiza.gsjulino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsjulinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
